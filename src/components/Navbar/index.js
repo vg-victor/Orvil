3c1c6926b5c18584602b5dd19c99ff9c1c4d6cc2
Victor Gabriel <vgvictor.ao@gmail.com>
2023-05-03 18:12:52 -0300

    navbar estilizada usando flexbox

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,55 +6,46 @@ const Navbar = ({ setPesquisa, local }) => {
   const [input, setInput] = useState("");
 
   return (
-    <nav className="navbar">
-      <div className="navbar-div">
-        <ol className="navbar-ol">
-          <div className="div-link-img-incial">
-            <Link className="navbar-link-inicial" to="/">
-              <img
-                className="navbar-img-inicial"
-                style={{ width: "45px", height: "45px" }}
-                src="orvil.ico"
-                alt="Página inicial"
-              />
-            </Link>
-          </div>
-          <div className="div-ipt-pesquisar-btn-buscar">
-            <input
-              onKeyDown={(e) =>
-                e.key === "Enter" ? setPesquisa(input) : setPesquisa("")
-              }
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              className="navbar-input-pesquisa"
-              placeholder="Pesquisar"
-            />
-            <button
-              className="navbar-button-buscar"
-              onClick={() => setPesquisa(input)}
-            >
-              Buscar
-            </button>
-          </div>
-          <div className="div-links-nvlivro-leitores">
-            {local === "listagemlivro" ? (
-              <>
-                <Link to="livro/0" className="navbar-link-novolivro">
-                  Novo livro
-                </Link>
-                <Link to="leitores" className="navbar-link-leitores">
-                  Leitores
-                </Link>
-              </>
-            ) : (
-              <Link to="/" className="navbar-link-leitores">
-                Livros
-              </Link>
-            )}
-          </div>
-        </ol>
+    <header className="header">
+      <Link to="/">
+        <img
+          style={{ width: "45px", height: "45px" }}
+          src="orvil.ico"
+          alt="Página inicial"
+          className="logo_img"
+        />
+      </Link>
+      <div className="pesquisar">
+        <input
+          onKeyDown={(e) =>
+            e.key === "Enter" ? setPesquisa(input) : setPesquisa("")
+          }
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="input_pesquisa"
+          placeholder="Pesquisar"
+        />
+        <button className="button_buscar" onClick={() => setPesquisa(input)}>
+          Buscar
+        </button>
       </div>
-    </nav>
+      <div className="links">
+        {local === "listagemlivro" ? (
+          <>
+            <Link to="livro/0" className="link">
+              Novo livro
+            </Link>
+            <Link to="leitores" className="link">
+              Leitores
+            </Link>
+          </>
+        ) : (
+          <Link to="/" className="link">
+            Livros
+          </Link>
+        )}
+      </div>
+    </header>
   );
 };
 
